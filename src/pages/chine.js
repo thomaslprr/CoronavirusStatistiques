a0ab07d330d83a7734d09db20504d5ce2b184194
Thomas Lapierre <thomaslapierre@orange.fr>
2020-03-15 12:39:02 +0100

    Création d'une deuxième page pour les statistiques chinois

---
+++ src/pages/chine.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const SecondPage = () => (
+  <Layout>
+    <SEO title="Statistique chine" />
+    <h1>Retrouvez les statistiques en chine</h1>
+
+    
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+export default SecondPage
