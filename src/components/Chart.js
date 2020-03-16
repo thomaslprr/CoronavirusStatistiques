4be398d6e67d7d299dca8a354385e61a7fdde062
Thomas Lapierre <thomaslapierre@orange.fr>
2020-03-16 18:19:15 +0100

    graphique responsive

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -30,8 +30,8 @@ class Chart extends Component{
     if(!this.state.loading){
       return (
 
-
-            <AreaChart width={window.innerWidth-200} height={400} data={this.state.resultat} margin={{ top: 10, right: 30, left: 0, bottom: 0 }} style={{margin : "auto"}}>
+          <div className="justify-content-center align-content-center cent">
+            <AreaChart width={window.innerWidth-200} height={400} data={this.state.resultat} margin={{ top: 10, right: 30, left: 0, bottom: 0 }} style={{margin : "auto", align: "center"}}>
               <CartesianGrid strokeDasharray="3 3" />
               <XAxis dataKey="reportDateString" />
               <YAxis />
@@ -39,6 +39,7 @@ class Chart extends Component{
               <Area type='monotone' dataKey='totalConfirmed' stroke='#ffc107' fill='#ffc107' />
               <Area type='monotone' dataKey='totalRecovered' stroke='#285d3a' fill='#48c774' />
             </AreaChart>
+          </div>
 
 
 
